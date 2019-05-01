84d90d96875d1fc36fc5b938761c1e84e4a62629
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-05-01 11:53:58 -0400

    working on local storage setup

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ loadEventListners();
 
 // Create load all event listner functions
 function loadEventListners(){
+  // DOM LOAD event
+  document.addEventListener('DOMContentLoaded', getTasks);
   // add events to tasks
   form.addEventListener('submit', addTask);
   // will use event delegation and add an event listner to the ul itself
@@ -42,9 +44,24 @@ function addTask(e){
   li.appendChild(link);
   // append the li to the ul
   taskList.appendChild(li);
+
+  //store in local storeage
+  storeTaskinLocalStorage(taskInput.value);
+
   // Clear input
   taskInput.value = '';
   e.preventDefault();
+}
+
+function storeTaskinLocalStorage(task){
+  let tasks;
+  if(localStorage.getItem('tasks') == null){
+    tasks = [];
+  }else{
+    tasks = JASON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function removeTask(){
@@ -71,6 +88,30 @@ function filterTasks(e){
       task.style.display = 'none';
     }
   });  // query selector all returns a node list so can use for each
+}
 
+// get tasks
+function getTasks(){
+  let tasks;
+  if(localStorage.getItem('tasks') == null){
+    tasks = [];
+  }else{
+    tasks = JASON.parse(localStorage.getItem('tasks'));
+  }
 
+  tasks.forEach(function(task){ // for each task we create the element
+    // create li element
+    const li = document.createElement('li');
+    // add the class
+    li.className = 'collection-item'; // in matteralize your ul should have a class of collection and li of collection-item
+    // create the text node and append to li
+    li.appendChild(document.createTextNode(task));
+    // create new link element
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content'; // if you want something on the right of a list item in materialize, need to be on the right of secondary content
+    // Add icon htmk
+    link.innerHTML = '<i class= "fas fa-minus-circle"></i>';
+    // append the link to the li
+    li.appendChild(link);
+  })
 }
